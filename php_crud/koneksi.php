4b50b46ef99c4d60c9025c63455f1ff9bc2a31a9
MVirdausZam <113084939+MVirdausZam@users.noreply.github.com>
2022-12-22 15:07:17 +0700

    Add files via upload

--- php_crud/koneksi.php
+++ php_crud/koneksi.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "myDB";
+$dbname = "formhandling";
 
 $connection = new mysqli($servername, $username, $password, $dbname);
 
